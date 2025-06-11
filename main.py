3120a0571fac73b15a4275e15f560ce62b2c9804
nandhi319 <nandhivardhan171@gmail.com>
2025-06-11 13:11:52 -0400

    Data Validation stage completed

---
+++ main.py
@@ -0,0 +1,26 @@
+from src.DS import logger
+from src.DS.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.DS.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+
+
+STAGE_NAME = "Data ingestion stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    data_ingestion  = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    data_validation  = DataValidationTrainingPipeline()
+    data_validation.initiate_data_validation()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
