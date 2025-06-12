dd0f228a595da0c43a40fe503e39a9bf81e37b63
nandhi319 <nandhivardhan171@gmail.com>
2025-06-12 17:28:14 -0400

    Model Evaluation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.DS.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipelin
 from src.DS.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.DS.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.DS.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.DS.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data ingestion stage"
 
@@ -47,3 +48,13 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/DS/config/configuration.py
+++ src/DS/config/configuration.py
@@ -1,12 +1,15 @@
 from src.DS.constants import *
 from src.DS.utils.common import read_yaml, create_directories
-from src.DS.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig)
+from src.DS.entity.config_entity import (DataIngestionConfig,DataValidationConfig,
+                                         DataTransformationConfig,ModelTrainerConfig,
+                                         ModelEvaluationConfig)
 
 class ConfigurationManager:
-    def __init__(self,
-                 config_filepath=CONFIG_FILE_PATH,
-                 params_filepath = PARAMS_FILE_PATH,
-                 schema_filepath = SCHEMA_FILE_PATH):
+    def __init__(
+        self,
+        config_filepath=CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH,
+        schema_filepath = SCHEMA_FILE_PATH):
         self.config =read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
         self.schema = read_yaml(schema_filepath)
@@ -71,3 +74,23 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config=self.config.model_evaluation
+        params=self.params.ElasticNet
+        schema=self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config=ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/nandhi319/DS_Project.mlflow"
+
+
+        )
+        return model_evaluation_config
